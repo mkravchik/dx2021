387932f26e37beefec454976caa99545887b9eed
Moshe Kravchik <mkravchik@hotmail.com>
2023-05-30 06:21:43 +0300

    Coping with large files without crashing

--- code2vec/prediction_outputter.py
+++ code2vec/prediction_outputter.py
@@ -26,41 +26,46 @@ def get_predictions(model, set_name, predictions_file, predictions_only=True):
 
     with open(json_file) as sample_file, open(context_paths) as contexts_file, open(predictions_file, "w") as predictions:
         for sample, function in zip(sample_file, contexts_file):
-            sample = json.loads(sample.strip())
-            if not predictions_only:
-                predictions.write(f"\n===================:\n{sample['project']}\n{sample['func'][:200]}\n===================:\n")
-            parts = function.rstrip().split(' ')
-            method_name = parts[0]
-            current_result_line_parts = [method_name]
-            contexts = parts[1:]
-
-            for context in contexts[:200]:
-                context_parts = context.split(',')
-                context_word1 = context_parts[0]
-                context_path = context_parts[1]
-                context_word2 = context_parts[2]
-                current_result_line_parts += ['%s,%s,%s' % (context_word1, context_path, context_word2)]
-            space_padding = ' ' * (200 - len(contexts))
-            result_line = ' '.join(current_result_line_parts) + space_padding
-            raw_prediction_results = model.predict([result_line])
-            method_prediction_results = common.parse_prediction_results(
-                    raw_prediction_results,
-                    model.vocabs.target_vocab.special_words, topk=SHOW_TOP_CONTEXTS)
-            # for raw_prediction, method_prediction in zip(raw_prediction_results, method_prediction_results):
-            #     predictions.write(f"{sample['idx']}\t{dicti[method_prediction.predictions[0]['name'][0]]}\n")
-            for raw_prediction, method_prediction in zip(raw_prediction_results, method_prediction_results):
-                # predictions.write(f"{sample['idx']}\t{dicti[method_prediction.predictions[0]['name'][0]]}\n")
-                # Raw predictions contain attentions for different contexts
-                # predictions.write(f"{sample['project']}\t{sample['func'][:40]}\tRaw:{raw_prediction}\tMethod:{method_prediction.predictions}\n")
-                # predictions.write(f"{method_prediction.predictions}\n")
-                predictions.write(json.dumps(method_prediction.predictions)+"\n")
+            try:
+                sample = json.loads(sample.strip())
                 if not predictions_only:
-                    predictions.write(' '.join(map(str, raw_prediction.code_vector)) + '\n')
-                code_vectors.append(raw_prediction.code_vector)
-                if not np.isnan(method_prediction.predictions[0]['probability']):
-                    labels.append(method_prediction.predictions[0]['name'][0])
-                else:
-                    labels.append("Unknown")
+                    predictions.write(f"\n===================:\n{sample['project']}\n{sample['func'][:200]}\n===================:\n")
+                parts = function.rstrip().split(' ')
+                method_name = parts[0]
+                current_result_line_parts = [method_name]
+                contexts = parts[1:]
+
+                for context in contexts[:200]:
+                    context_parts = context.split(',')
+                    context_word1 = context_parts[0]
+                    context_path = context_parts[1]
+                    context_word2 = context_parts[2]
+                    current_result_line_parts += ['%s,%s,%s' % (context_word1, context_path, context_word2)]
+                space_padding = ' ' * (200 - len(contexts))
+                result_line = ' '.join(current_result_line_parts) + space_padding
+                raw_prediction_results = model.predict([result_line])
+                method_prediction_results = common.parse_prediction_results(
+                        raw_prediction_results,
+                        model.vocabs.target_vocab.special_words, topk=SHOW_TOP_CONTEXTS)
+                # for raw_prediction, method_prediction in zip(raw_prediction_results, method_prediction_results):
+                #     predictions.write(f"{sample['idx']}\t{dicti[method_prediction.predictions[0]['name'][0]]}\n")
+                for raw_prediction, method_prediction in zip(raw_prediction_results, method_prediction_results):
+                    # predictions.write(f"{sample['idx']}\t{dicti[method_prediction.predictions[0]['name'][0]]}\n")
+                    # Raw predictions contain attentions for different contexts
+                    # predictions.write(f"{sample['project']}\t{sample['func'][:40]}\tRaw:{raw_prediction}\tMethod:{method_prediction.predictions}\n")
+                    # predictions.write(f"{method_prediction.predictions}\n")
+                    predictions.write(json.dumps(method_prediction.predictions)+"\n")
+                    if not predictions_only:
+                        predictions.write(' '.join(map(str, raw_prediction.code_vector)) + '\n')
+                    code_vectors.append(raw_prediction.code_vector)
+                    if not np.isnan(method_prediction.predictions[0]['probability']):
+                        labels.append(method_prediction.predictions[0]['name'][0])
+                    else:
+                        labels.append("Unknown")
+            except Exception as e:
+                print(e)
+                print(f"Error in sample: {sample}")
+                print(f"Error in function: {function}")
         if not predictions_only:
             #count the labels of each class
             labels_counts = np.unique(np.array(labels), return_counts=True)
--- extract_asts.sh
+++ extract_asts.sh
@@ -7,7 +7,32 @@ fi
 
 cp ./$1.jsonl astminer/dataset/
 
-echo Extracting ASTs from $1
-cd astminer
-./cli.sh $1 0
-cd ..
+# if the file astminer/dataset/$1.jsonl is newer than code2vec/devign.$1.raw txt file or there is no code2vec/devign.$1.raw txt, then we need to extract ASTs
+if [ -f "code2vec/devign.$1.raw.txt" ] || [ "astminer/dataset/$1.jsonl" -nt "code2vec/devign.$1.raw.txt" ]; then
+    echo Extracting ASTs from $1
+
+    # Split the file into chunks of 10000 lines
+    cd astminer/dataset
+    split -l 10000 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".jsonl" "$1.jsonl" "$1."
+    # Process each split file with cli.sh
+    for file in $1.*.jsonl; do
+        if [[ $file =~ $1\.[0-9]+\.jsonl ]]; then
+            # Remove the .jsonl extension
+            base=$(basename "$file" .jsonl)
+
+            echo Extracting ASTs from $base
+            cd ..
+            ./cli.sh "$base" 0
+            cd dataset
+        fi
+    done
+
+    # Combine all output files into a single file
+    cd ../../code2vec
+    cat devign.$1.*.raw.txt > devign.$1.raw.txt
+
+    # Remove the temporary raw files
+    rm devign.$1.*.raw.txt
+
+    cd ..
+fi
